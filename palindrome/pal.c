36b57a49cd6e1c076c4e57c2c7b931d55f4a7927
Khang Tran <ktran7246@gmail.com>
2024-04-12 18:24:32 -0500

    crying more

--- palindrome/pal.c
+++ palindrome/pal.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 int main() {
-  char *str = fgets(str, 1024, stdin);
-  char *start = str;
-  char *end = str;
-  while (*end != '\0' && *end != '\n') {
+  char str[1024];
+  fgets(str, 1024, stdin);
+  int start = 0;
+  int end = 0;
+  while (str[start] != '\0' && str[end] != '\n') {
     end++;
   }
   end--;
   int isPalindrome = 0;
   while (start < end) {
-    if (*start != *end) {
+    if (str[start] != str[end]) {
       isPalindrome = 1;
       break;
     }
